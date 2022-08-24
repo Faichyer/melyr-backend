a799c408a1dd55c3cca5382f8225f9ed88aa1b49
Faichyer <miki.namsrai@gmail.com>
2022-08-24 18:03:05 +0200

    config mongoose

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,12 @@
 import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+    mongo: {
+        url: process.env.MONGO_URL || ''
+    },
+    server: {
+        port: process.env.PORT ? Number(process.env.PORT) : 1337
+    }
+};
